590d7266841736b126fb5f407904a82373c76704
Alex Alvarez <alex.alvarez1903@gmail.com>
2024-04-09 13:36:28 -0700

    Initial Commit

---
+++ MacroMonkey/MacroMonkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroMonkeyApp.swift
+//  MacroMonkey
+//
+//  Created by Alex Alvarez on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MacroMonkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
